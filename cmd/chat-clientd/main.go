a1a82e164ba684a6db38a0bfcccc91e80641d638
alkuinvito <alkuinvito@tutanota.com>
2025-07-22 19:33:11 +0700

    feat: add registration as entrypoint

--- cmd/chat-clientd/main.go
+++ cmd/chat-clientd/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"github.com/alkuinvito/chat-client/internal/auth"
+	"github.com/alkuinvito/chat-client/pkg/db"
 	"github.com/alkuinvito/chat-client/pkg/views"
 )
 
 func main() {
+	db := db.NewDB()
+
 	a := app.New()
-	view := views.NewView(a, "P2P Chat")
+	view := views.NewView(a, "P2P Chat", db)
+	view.Window().Resize(fyne.NewSize(900, 600))
+
+	// register new user as entrypoint
+	view.Render(auth.RegisterView)
 
 	view.Run()
 }
---
+++ internal/auth/auth_view.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"errors"
+	"regexp"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/alkuinvito/chat-client/pkg/views"
+)
+
+func RegisterView(v *views.View) fyne.CanvasObject {
+	alphanum := regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+	username := widget.NewEntry()
+
+	username.Validator = func(s string) error {
+		if len(s) < 3 || len(s) > 32 {
+			return errors.New("username must be between 3 and 32 characters")
+		}
+
+		if !alphanum.MatchString(s) {
+			return errors.New("username must only contains alphanumeric")
+		}
+
+		return nil
+	}
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			widget.NewFormItem("Username", username),
+		},
+		SubmitText: "Register",
+		OnSubmit:   func() {}}
+
+	gridCt := container.NewGridWrap(fyne.NewSize(400, form.MinSize().Height), form)
+	ct := container.NewCenter(gridCt)
+
+	return ct
+}
