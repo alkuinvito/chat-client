6654399108417808b7447140a59d765bbbe59e9e
alkuinvito <alkuinvito@tutanota.com>
2025-07-22 19:35:39 +0700

    build: add db as view properties

--- pkg/views/views.go
+++ pkg/views/views.go
@@ -2,16 +2,22 @@ package views
 
 import (
 	"fyne.io/fyne/v2"
+	"gorm.io/gorm"
 )
 
 type View struct {
-	a fyne.App
-	w fyne.Window
+	a  fyne.App
+	w  fyne.Window
+	db *gorm.DB
 }
 
-func NewView(a fyne.App, title string) *View {
+func NewView(a fyne.App, title string, db *gorm.DB) *View {
 	w := a.NewWindow(title)
-	return &View{a, w}
+	return &View{a, w, db}
+}
+
+func (v *View) DB() *gorm.DB {
+	return v.db
 }
 
 func (v *View) Render(view func(*View) fyne.CanvasObject) {
