9224bf7d11769e53051e6be544447fb65d5492bd
alkuinvito <alkuinvito@tutanota.com>
2025-08-01 00:07:21 +0700

    feat: revamp login & register pages

--- frontend/src/models.ts
+++ frontend/src/models.ts
@@ -5,7 +5,29 @@ export type TResponseSchema<T> = {
   data: T;
 };
 
-export const RegisterSchema = z.object({
+export const RegisterSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "Username must between 3 to 16 characters")
+      .max(16, "Username must between 3 to 16 characters")
+      .regex(/^[a-zA-Z0-9]+$/, {
+        message: "Username must be alphanumeric",
+      }),
+    password: z
+      .string()
+      .min(8, "Password must between 8 to 32 characters")
+      .max(32, "Password must between 8 to 32 characters"),
+    confirm_password: z.string(),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Passwords do not match",
+    path: ["confirm_password"],
+  });
+
+export type TRegisterSchema = z.infer<typeof RegisterSchema>;
+
+export const LoginSchema = z.object({
   username: z
     .string()
     .min(3, "Username must between 3 to 16 characters")
@@ -19,7 +41,7 @@ export const RegisterSchema = z.object({
     .max(32, "Password must between 8 to 32 characters"),
 });
 
-export type TRegisterSchema = z.infer<typeof RegisterSchema>;
+export type TLoginSchema = z.infer<typeof LoginSchema>;
 
 export type TProfileSchema = {
   id: string;
--- frontend/src/pages/UserRegister.tsx
+++ frontend/src/pages/UserRegister.tsx
@@ -19,7 +19,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
-import { Info } from "lucide-react";
+import { Info, LockKeyhole, LogInIcon, WifiOff } from "lucide-react";
+import logo from "@/assets/images/logo.png";
+import { BrowserOpenURL } from "../../wailsjs/runtime";
 
 function UserRegister() {
   const navigate = useNavigate();
@@ -42,47 +44,100 @@ function UserRegister() {
           });
         }
       })
-      .catch((e) => {});
+      .catch(() => {});
   }
 
   return (
-    <MainLayout className="flex flex-col justify-center gap-16">
-      <div>
-        <h1 className="text-4xl font-bold select-none">Chat Client</h1>
-      </div>
-      <div className="mx-auto">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="grid gap-4 w-screen max-w-80"
-          >
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder="Username" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input type="password" placeholder="Password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Register</Button>
-          </form>
-        </Form>
+    <MainLayout>
+      <div className="grow flex flex-col md:flex-row gap-8 items-center justify-center w-full h-full mx-8">
+        <div className="max-w-80 w-full float-right clear-both">
+          <div>
+            <h1 className="mb-1 text-4xl font-bold select-none">Chat Client</h1>
+            <div className="flex items-center justify-center gap-3">
+              <span className="text-xs text-neutral-500">
+                another project from
+              </span>
+              <button
+                className="outline-none border-none text-sm flex gap-1 items-center"
+                onClick={() => {
+                  BrowserOpenURL("https://wham.my.id");
+                }}
+              >
+                <img src={logo} className="size-6" /> <span>wham.my.id</span>
+              </button>
+            </div>
+          </div>
+
+          <ul className="grid grid-rows-2 gap-4 mt-8">
+            <li className="flex items-center justify-start gap-3 p-3 border border-blue-900/50 bg-blue-900/30 text-blue-400/80 text-left text-sm rounded-lg">
+              <WifiOff />
+              <span>No need for internet, just LAN.</span>
+            </li>
+            <li className="flex items-center justify-start gap-3 p-3 border border-blue-900/50 bg-blue-900/30 text-blue-400/80 text-left text-sm rounded-lg">
+              <LockKeyhole />
+              <span>End-to-end encrypted chat.</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="max-w-80 w-full mx-auto md:mx-0 p-4 border border-neutral-700 bg-neutral-800 rounded-lg">
+          <h3 className="text-xl mb-4">Register your new identity</h3>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid gap-4 w-full"
+            >
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirm_password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Confirm password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">
+                <LogInIcon />
+                Register
+              </Button>
+            </form>
+          </Form>
+        </div>
       </div>
     </MainLayout>
   );
